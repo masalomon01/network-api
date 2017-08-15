fbc8175a1afac1dd927d7274f3cc9a14fb40d191
masalomon <salermom@gmail.com>
2017-08-14 18:26:20 -0700

    adding embedded documentation

--- app/main/views.py
+++ app/main/views.py
@@ -4,4 +4,4 @@ from flask import flash, redirect, render_template, session, url_for, abort
 
 @main.route("/")
 def index():
-    return "NETWORK API is running!"
+    return '<iframe src="https://docs.google.com/document/d/11uYJ9BZKwesX4Mby7RkXV8SYH8X3PuQyI07_KBzkYSg/pub?embedded=true"></iframe>'
