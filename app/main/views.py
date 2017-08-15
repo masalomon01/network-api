3f34834a26516857e438d25a702648526ae23255
masalomon <salermom@gmail.com>
2017-08-14 18:29:01 -0700

    fixing doc height and width

--- app/main/views.py
+++ app/main/views.py
@@ -4,4 +4,4 @@ from flask import flash, redirect, render_template, session, url_for, abort
 
 @main.route("/")
 def index():
-    return '<iframe src="https://docs.google.com/document/d/11uYJ9BZKwesX4Mby7RkXV8SYH8X3PuQyI07_KBzkYSg/pub?embedded=true"></iframe>'
+    return '<iframe style="width: 100%; height: 100%;" src="https://docs.google.com/document/d/11uYJ9BZKwesX4Mby7RkXV8SYH8X3PuQyI07_KBzkYSg/pub?embedded=true"></iframe>'
