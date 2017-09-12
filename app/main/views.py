fd0950ddcd066f8e5058899851f6fe6e4164ceae
masalomon <salermom@gmail.com>
2017-09-11 18:03:58 -0700

    add loader.io verification fix

--- app/main/views.py
+++ app/main/views.py
@@ -6,7 +6,8 @@ from flask import flash, redirect, render_template, session, url_for, abort, sen
 def index():
 	return '<iframe style="width: 100%; height: 100%;" src="https://docs.google.com/document/d/11uYJ9BZKwesX4Mby7RkXV8SYH8X3PuQyI07_KBzkYSg/pub?embedded=true"></iframe>'
 
-# app = Flask(__name__, main='main')
-@main.route("/loaderio-a7fd191577702f8371e9f349d4958a61.txt")
-def txt_file():
-	return redirect(url_for('main.txt_file', filename='loaderio-a7fd191577702f8371e9f349d4958a61.txt'))
+
+@main.route("/loaderio-a7fd191577702f8371e9f349d4958a61.html")
+def html_file():
+	#return render_template(url_for('static', filename='loaderio-a7fd191577702f8371e9f349d4958a61.html'))
+	return render_template('loaderio-a7fd191577702f8371e9f349d4958a61.html')
